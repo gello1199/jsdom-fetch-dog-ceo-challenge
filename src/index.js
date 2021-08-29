97ca45a7dbec3274a32ae8324714e5570667f218
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-08-29 15:55:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+const imageContainer = document.getElementById(`dog-image-container`);
+const listContainer = document.getElementById(`dog-breeds`);
+
+document.addEventListener("DOMContentLoaded", function() {
+    dogImages()
+    dogBreeds()
+
+});
+
+
+
+function dogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then(response => response.json())
+      .then(data => data.message.forEach(image => addPic(image)));
+}
+
+function addPic(image) {
+    const newElement = document.createElement('img'); 
+    newElement.src = image;
+    imageContainer.appendChild(newElement);
+}
+
+function dogBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+        for(const breed in data.message)
+        addBreed(breed);
+    });
+}
+
+function addBreed(breed) {
+    const breedLi = document.createElement('li');
+    breedLi.innerHTML = breed;
+    listContainer.appendChild(breedLi);
+
+    breedLi.addEventListener(`click`, function(e) {
+        e.target.style.color = `red;`
+    });
+};
+
